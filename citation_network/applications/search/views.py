01aba1de150a7d03ab7a093eb736a7b7ca9945d3
Axelssskmt <ychang0212@gmail.com>
2021-12-22 20:22:04 +0800

    完成排序

--- citation_network/applications/search/views.py
+++ citation_network/applications/search/views.py
@@ -99,5 +99,70 @@ def relation_graph(request):
     return render(request, "index.html")
 
 def sort_by_rank(request):#按照重要性分数排序
+    key_words = request.GET.get('query')  # 接收一个query变量，query包含了输入框的词，以此返回给elasticsearch做分词匹配
+    paper_count = int(client.count(index="otrap")["count"])
+    page = request.GET.get("page")
+    try:
+        page = int(page)
+    except:
+        page = 1
+    start_time = datetime.now()
+    response = client.search(
+        index="otrap",
+        body={
+            'query': {
+                "multi_match": {
+                    "query": key_words,
+                    "fields": ["title"]
+                }
+            },
+            "from": (page - 1) * 10,
+            "size": 10,
+            "sort": [
+                {
+                    "score": "desc"
+                }
+            ],
+            "highlight": {  # 词语高亮
+                "pre_tags": ["<span class='keyWord'>"],
+                "post_tags": ["</span>"],
+                "fields": {
+                    "title": {},
+                }
+            }
+        }
+    )
+    print(response)
+    end_time = datetime.now()
+    last_seconds = (end_time - start_time).total_seconds()
+    total_nums = response["hits"]["total"]
+    if (page % 10) > 0:  # 总页数
+        page_nums = int(total_nums / 10) + 1
+    else:
+        page_nums = int(total_nums / 10)
+    hit_list = []
+    for hit in response['hits']['hits']:
+        hit_dict = {}
+        if "title" in hit["_source"]:
+            hit_dict["title"] = hit["_source"]["title"]
+        if "Sid" in hit["_source"]:
+            hit_dict["Sid"] = hit["_source"]["Sid"]
+        if "year" in hit["_source"]:
+            hit_dict["year"] = int(hit["_source"]["year"])
+        if "inCitationsCount" in hit["_source"]:
+            hit_dict["inCitationsCount"] = int(hit["_source"]["inCitationsCount"])
+        if "outCitationsCount" in hit["_source"]:
+            hit_dict["outCitationsCount"] = int(hit["_source"]["outCitationsCount"])
+        if "score" in hit["_source"]:
+            hit_dict["score"] = float(hit["_source"]["score"])
 
-    return render(request, 'homepage.html')
+        hit_list.append(hit_dict)
+        print(hit_list)
+    # return render(request, "result.html", {"all_list": hit_list,
+    #                                        "key_words": key_words,
+    #                                        "total_nums": total_nums,
+    #                                        "page_nums": page_nums,
+    #                                        "last_seconds": last_seconds,
+    #                                        "page": page,
+    #                                        "paper_count": paper_count})
+    return JsonResponse(hit_list, safe=False)
