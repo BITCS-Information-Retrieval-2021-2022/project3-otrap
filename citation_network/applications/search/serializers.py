a22e57c378c8eaebd6bb72a53f6e68a4677046c4
Axelssskmt <38099407+Axelssskmt@users.noreply.github.com>
2021-12-25 22:20:09 +0800

    Delete serializers.py

--- citation_network/applications/search/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import PaperType
-
-class PaperSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PaperType
-        fields = '__all__'
